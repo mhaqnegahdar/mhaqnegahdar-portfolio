9b1ef9db8610249fb6af9090e19638569c8a910e
mhaqnegahdar <mhaqnegahdar@gmail.com>
2025-02-08 08:31:13 +0330

    Contact Form & Accessibility fixes: - Fixed minor Accessibility issues - Added image kit as image provider - Connected Contact form to Telegram bot

--- src/components/layout/sections/projects/Project.tsx
+++ src/components/layout/sections/projects/Project.tsx
@@ -2,7 +2,8 @@
 
 import { useRef } from "react";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
+import Image from "@/components/ui/Image";
+
 import { motion, useScroll, useTransform } from "framer-motion";
 import Link from "next/link";
 
@@ -14,7 +15,7 @@ export default function Project({
   tags,
   imageUrl,
   demoLink,
-  codeLink
+  codeLink,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -36,13 +37,13 @@ export default function Project({
       <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[23rem] sm:pr-8 sm:group-even:pl-8">
         <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 mb-4 leading-relaxed text-gray-700 dark:text-white/90">
+          <p className="mb-4 mt-2 leading-relaxed text-gray-700 dark:text-white/90">
             {description}
           </p>
           <ul className="mb-4 flex flex-wrap gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
-                className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white "
+                className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white"
                 key={index}
               >
                 {tag}
@@ -68,15 +69,13 @@ export default function Project({
         </div>
 
         <Image
-          src={imageUrl}
+          path={imageUrl}
           alt="Project I worked on"
-          width={"400"}
-          height={"300"}
-          quality={95}
+          w={800}
+          h={450}
           className="absolute -right-40 top-8 hidden w-[28.25rem] rounded-t-lg shadow-2xl transition group-even:-left-40 group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] sm:block"
         />
       </section>
     </motion.div>
   );
 }
-
