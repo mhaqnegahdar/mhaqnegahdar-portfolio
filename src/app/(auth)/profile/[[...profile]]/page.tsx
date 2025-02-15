d3f4c5aab4c979cc792409f50dd038d1ebf3fe48
mhaqnegahdar <mhaqnegahdar@gmail.com>
2025-02-15 20:14:17 +0330

    RePush

--- src/app/(auth)/profile/[[...profile]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserProfile } from "@clerk/nextjs";
-
-export default function UserProfilePage() {
-  return (
-    <div className="flex items-center justify-center py-8">
-      <UserProfile path="/profile" />
-    </div>
-  );
-}
