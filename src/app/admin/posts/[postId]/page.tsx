543f18cb3dbe9cb64463c160e00a1f171126c6cf
mhaqnegahdar <mhaqnegahdar@gmail.com>
2025-02-15 20:27:00 +0330

    Fix lint error

--- src/app/admin/posts/[postId]/page.tsx
+++ src/app/admin/posts/[postId]/page.tsx
@@ -4,51 +4,53 @@ import React from "react";
 import PostForm from "./components/PostForm";
 
 //Types
-import { Category, FetchedPost } from "@/types/models";
+// import { Category, FetchedPost } from "@/types/models";
 
 export default function CreateArticlePage() {
-  const mockArticle: FetchedPost = {
-    id: "1",
-    userId: "1",
-    title: "Post",
-    image: "https://picsum.photos/200/300",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod, lectus at ultrices malesuada, justo libero tincidunt purus, vitae tincidunt nunc nibh a erat. Morbi nec lectus id lectus malesuada eleifend. Nulla ut lectus auctor, egestas magna quis, mattis lectus. Sed et nulla auctor, dictum nibh nec, convallis nunc. Nulla facilisi. Nulla in diam eu magna tincidunt laoreet. Sed nec lectus id lectus malesuada eleifend. Nulla ut lectus auctor, egestas magna quis, mattis lectus. Sed et nulla auctor, dictum nibh nec, convallis nunc.",
-    status: "draft",
-    isFeatured: false,
-    readTime: "5 min read",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    categories: [
-      { id: "1", title: "Next.js" },
-      { id: "2", title: "React" },
-    ],
-  };
+  // const mockArticle: FetchedPost = {
+  //   id: "1",
+  //   userId: "1",
+  //   title: "Post",
+  //   image: "https://picsum.photos/200/300",
+  //   content:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod, lectus at ultrices malesuada, justo libero tincidunt purus, vitae tincidunt nunc nibh a erat. Morbi nec lectus id lectus malesuada eleifend. Nulla ut lectus auctor, egestas magna quis, mattis lectus. Sed et nulla auctor, dictum nibh nec, convallis nunc. Nulla facilisi. Nulla in diam eu magna tincidunt laoreet. Sed nec lectus id lectus malesuada eleifend. Nulla ut lectus auctor, egestas magna quis, mattis lectus. Sed et nulla auctor, dictum nibh nec, convallis nunc.",
+  //   status: "draft",
+  //   isFeatured: false,
+  //   readTime: "5 min read",
+  //   createdAt: new Date(),
+  //   updatedAt: new Date(),
+  //   categories: [
+  //     { id: "1", title: "Next.js" },
+  //     { id: "2", title: "React" },
+  //   ],
+  // };
 
-  const mockCategories: Category[] = [
-    {
-      id: "1",
-      title: "Next.js",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: "2",
-      title: "React",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: "3",
-      title: "TypeScript",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ];
+  // const mockCategories: Category[] = [
+  //   {
+  //     id: "1",
+  //     title: "Next.js",
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "React",
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  //   {
+  //     id: "3",
+  //     title: "TypeScript",
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // ];
 
   return (
     <>
-      <PostForm post={mockArticle} categories={mockCategories} />
+      <PostForm 
+      // post={mockArticle} categories={mockCategories} 
+      />
     </>
   );
 }
